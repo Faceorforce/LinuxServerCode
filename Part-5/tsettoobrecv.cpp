717f95267721fa0cbd6747efcd584fbba5c92d12
Faceorforce <1808248575@qq.com>
2021-07-04 13:54:01 -0400

    master

---
+++ Part-5/tsettoobrecv.cpp
@@ -0,0 +1,16 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    if (args <= 2)
+    {
+        printf("usage")
+    }
+}
